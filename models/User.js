489efdd87656c972dd52caceb691ea8e2d9961ae
정효진 <jeonghyojin@keunhyojing.local>
2022-01-28 19:06:21 +0900

    비밀번호 암호화 시키기

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -28,6 +29,27 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+})
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')) {
+
+        //비밀번호를 암호화 시키기
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
     }
 })
 
